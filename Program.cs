10a33d57854cb578d7706a62f07cf613464f2599
maverickwwq <maverickwwq@gmail.com>
2021-02-23 16:12:02 +0800

    2021/2/23 16:11

--- Program.cs
+++ Program.cs
@@ -51,26 +51,30 @@ namespace svrSimu
             
             while (true)
             {
+                string a = @"{""CommType"": ""DOWN_ORDER"",""CommTime"": ""2021/1/14 16:49:31"",""CommDept"": ""7830"",  ""order"": {    ""orderId"": 68895,    ""orderCode"": ""广无调单字【2021】012430号"",    ""orderOpList"": [      {        ""ID"": 260864,        ""orderId"": 68895,        ""orderYear"": 2021,        ""orderCode"": ""广无调单字【2021】012430号"",        ""sourceType"": ""正常任务"",        ""orderStatus"": ""50"",        ""orderStatusDate"": ""2021/1/14 15:02:00"",        ""sender"": ""沈如梅"",        ""sendDate"": ""2021/1/14 15:01:57"",        ""sendAccessor"": ""无线局运行调度中心"",        ""sendDept"": ""频调处"",        ""receiver"": ""系统"",        ""receiveDate"": ""2021/1/14 15:02:00"",        ""corrector"": """",        ""correctDate"": """",        ""orderRmks"": """",        ""createType"": ""N"",        ""orderFormat"": ""ORDR"",        ""flagReply"": ""N"",        ""replyDate"": """",        ""insteadFlag"": ""N"",        ""insteadId"": -1,        ""orderNumId"": 102210,        ""num"": 1,        ""orderType"": ""实验"",        ""transCode"": ""A01"",        ""antennaCode"": ""109"",        ""startDate"": ""2021/1/14 0:00:00"",        ""endDate"": ""2021/1/14 0:00:00"",        ""startTime"": ""1980/1/1 15:00:00"",        ""endTime"": ""1980/1/1 16:00:00"",        ""freq"": 10160,        ""power"": 100,        ""programCode"": ""CNR00100000"",        ""programName"": ""中一"",        ""azimuthM"": ""27"",        ""azimuthDe"": """",        ""operate"": ""开"",        ""target"": -1,        ""servArea"": ""15"",        ""days"": ""4"",        ""msgNum"": """",        ""channel"": ""1"",        ""season"": """",        ""mod"": """",        ""opRmks"": """",        ""opStatus"": ""80"",        ""opStatusDate"": ""2021/1/14 15:02:02"",        ""inexeReasonCode"": """",        ""inexePerson"": """",        ""antProg"": ""146"",        ""broadcastTime"": """",        ""innerTaskInsteadInfo"": """",        ""insteadStationCode"": """",        ""insteadTransUsedCode"": """",        ""updateTime"": ""2021/1/14 15:02:02"",        ""dealed"": 1,        ""operation"": ""U""      }    ],    ""orderRecordList"": [      null    ]  },  ""ifRequestSucess"": false}";
+                a = Regex.Replace(a, @"\s", "");
+                Console.WriteLine(a.Length);
                 s=Console.ReadLine();
                 if (s.Contains("a"))       //receive_order_reply 
                 {
+                    /*
                     RSData  d = new RSData ();
-                    d.CommType = "RECEIVE_ORDER_REPLY";
+                    d.CommType = "DOWN_ORDER";
                     d.CommTime = System.DateTime.Now.ToUniversalTime().ToString();
                     d.CommDept = "7832";
                     d.order = new Order(28362023, 2020, "0154041", "正常调度令", "待下发",
                         "2020/05/20 12:29:30", "董晓萌", "2020/05/20 12:27:30", "郝佳", "无线局调度中心",
                         "王伟强", "2020/05/20 12:32:00", "校对人", "校对时间", "备注", "", "", "", "", "", "", "", "", 0);
                     d.infoReturn = "当班人员已接收";
-                    string a = Newtonsoft.Json.JsonConvert.SerializeObject(d, Formatting.Indented, setting) ;
+                    a = Newtonsoft.Json.JsonConvert.SerializeObject(d, Formatting.Indented, setting) ;*/
                     GlobalVarForApp.sendMessageQueue.Enqueue(a);
                     network.sendData();
                     Console.WriteLine("case a");
-                    Console.WriteLine("send {0}",  a);
-                    
+                    //Console.WriteLine("send {0}",  a);
                 }
                 if (s.Contains("b"))                //feed_back_reply
                 {
+                    /*
                     RSData d = new RSData();
                     d.CommType = "FEED_BACK_REPLY";
                     d.CommTime = System.DateTime.Now.ToUniversalTime().ToString();
@@ -83,8 +87,11 @@ namespace svrSimu
                 6230, 450, 1, "369", "", "HR2/0.5/4", "1", 0, "西藏", "1234567", "", "1", "", "", "", "", "", "", "", "", "", "", ""),
                 new OrderOp(28362024, 28361019, 1, "正常调度令", 1, 1, "2020/05/15 0:0:0", "2020/10/27 0:0:0", "08:00:00", "10:00:00",
                 6230, 100,1, "313", "", "HR2/0.5/4", "1", 0, "西藏", "1234567", "", "1", "", "", "", "", "", "", "", "", "", "", "") });
-                    d.orderRecordList = new List<OrderRecord>(new OrderRecord[] { new OrderRecord() });
-                    string a = Newtonsoft.Json.JsonConvert.SerializeObject(d, Formatting.Indented, setting);
+                    d.orderRecordList = new List<OrderRecord>(new OrderRecord[] { new OrderRecord() });*/
+                    //string a = Newtonsoft.Json.JsonConvert.SerializeObject(d, Formatting.Indented, setting);
+
+                    //string a = @"asdfdbjfkbjtr/sgrigjprjgg'""";
+                    Console.WriteLine(a);
                     GlobalVarForApp.sendMessageQueue.Enqueue(a);
                     network.sendData();
                     Console.WriteLine("case b");
--- RSData.cs
+++ RSData.cs
@@ -8,34 +8,30 @@ using DispatchServer.BaseClass;
 namespace DispatchServer
 {
     public class RSData
-    {
-        
+    {        
         public string CommType = "";//通讯类型的定义
         public string CommTime = "";//通讯时间的定义
-        public string CommDept = "";//通讯机房名称：甲机房7830、乙机房7831，中控机房7850
+        public string CommDept = "";//通讯机房代码定义
         public User currentUser = null;//当前用户
 
         //连接状态
         public ConnectState connectState = null;
 
         //设置相关的操作变量
-        public User user = null;
-        public List<User> userList = null;
+        public User user = null;//用户(单个)
+        public List<User> userList = null;//用户(多个)
         public string infoReturn=null;//操作信息返回
 
         //调令相关的操作变量
-        public Order order = null;//下发的调度令
-        public List<OrderOp> orderOpList = null;//调度指令集
-        public List<OrderRecord> orderRecordList = null;//调令跟踪记录
-        public List<OrderAndOp> orderAndOpList = null;//调令综合信息
-        public string newMessage = null;//新消息提醒条数
+        public Order order = null;//调度令(单个)
+        public List<Order> orderList = null;//调度令(多个)
+        public string newMessage = null;//新消息提醒内容
 
         //查询相关
         public Query query = null;//相关查询变量
 
         //系统标识
         public bool ifRequestSucess = false;//请求操作是否成功
-       
 
         public RSData()
         {
